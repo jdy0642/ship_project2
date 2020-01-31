44a87af0ec481b493ce84d66499b80fce1c3f540
jdy0642 <jdy0642@gmail.com>
2020-01-31 19:41:57 +0900

    19:41
    19:41

--- src/main/java/com/ship/web/lol/LolController.java
+++ src/main/java/com/ship/web/lol/LolController.java
@@ -14,9 +14,11 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,6 +45,21 @@ public class LolController {
 	public ArrayList<HashMap<String, String>> opgg(@PathVariable String summonername){
 		System.out.println("방생성 시 크롤링 db 데이터 진입"+summonername);
 		return crawler.opggCrawling(summonername);
+	}
+	
+	@DeleteMapping("/delete/{cardseq}")
+	public void withdrawal(@PathVariable Long cardseq) {
+		p.accept("방탈 진입");
+		lolRepository
+		.delete(lolRepository
+				.findByCardseq(cardseq));
+	}
+	@PutMapping("/modify/{cardseq}")
+	public void modify(@PathVariable Long cardseq) {
+		p.accept("방탈 진입");
+		lolRepository
+		.delete(lolRepository
+				.findByCardseq(cardseq));
 	}
 	
 	@GetMapping("/listpage={page}")
