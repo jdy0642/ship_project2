3dc6ce481764731a970fac411a4013c1975e1a69
jdy0642 <jdy0642@gmail.com>
2020-02-13 20:12:45 +0900

    02 13

--- src/main/java/com/ship/web/util/Constants.java
+++ src/main/java/com/ship/web/util/Constants.java
@@ -2,8 +2,8 @@ package com.ship.web.util;
 
 public class Constants {
 	public static final String
-		LOCAL = "http://shipteamdb.s3-website.ap-northeast-2.amazonaws.com",
-		//LOCAL = "http://localhost:8081",
+		//LOCAL = "http://shipteamdb.s3-website.ap-northeast-2.amazonaws.com",
+		LOCAL = "http://localhost:8081",
 		KAKAO = "http://kapi.kakao.com",
 		J_S3 = "http://shipteamdb.s3-website.ap-northeast-2.amazonaws.com",
 		S_S3 = "http://shipteamdb7.s3-website.ap-northeast-2.amazonaws.com",
